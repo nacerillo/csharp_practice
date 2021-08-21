b78f3e8b1f0fa8d075ece66f88eeb88fa7531f84
nacerillo <nicholascerillo@gmail.com>
2021-08-21 12:36:21 -0400

    created human class with constructor

---
+++ ClassPractice/ClassPractice/Human.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice
+{
+    //blueprint for a data type
+    class Human
+    {
+        //member variable
+        private string firstName;
+        private string lastName = "Harris";
+        private int age;
+        private string eyeColor;
+
+        //constructor = 
+        public Human(string firstName, string lastName, int age, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.eyeColor = eyeColor;
+        }
+
+
+        //member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hello there! my name is {0} {1}, and I am {2} years old", firstName, lastName, age);
+        }
+
+    }
+}
---
+++ ClassPractice/ClassPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassPractice
+{
+    // a class is a blueprint of an object
+    // it has actions/abilites - so called member functions
+    // it has properties
+    // and it can be inherited
+
+    // object:
+    // - also has properties
+    // 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //in instance of a human class
+            Human denis = new Human("Jack", "Cerillo", 24, "blue");
+            //how to access a public variable
+            //denis.firstName = "Denis";
+            //denis.lastName = "Haufman";
+
+            Human nick = new Human("Nicholas", "Cerillo", 27, "brown");
+            //nick.firstName = "Nicholas";
+            //nick.lastName = "Cerillo";
+            denis.IntroduceMyself();
+            nick.IntroduceMyself();
+            Console.ReadKey();
+           //Console.WriteLine("Hello World!");
+        }
+    }
+}
